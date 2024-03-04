5e99563e637c838ba21beb9bdd2eef38a13648ec
Md-Anjir <104209904+Md-Anjir@users.noreply.github.com>
2024-03-04 14:09:50 +0600

    Adding progress file

---
+++ src/kids_learner/ProgressNumber.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Kids_learner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ProgressNumber {
+    
+    public static int wrong_no_add = 0;
+    public static int correct_no_add = 0;
+    
+    public static int wrong_no_sub = 0;
+    public static int correct_no_sub = 0;
+    
+    public static int wrong_no_mul = 0;
+    public static int correct_no_mul = 0;
+    
+    public static int wrong_no_div = 0;
+     public static int correct_no_div = 0;
+}
